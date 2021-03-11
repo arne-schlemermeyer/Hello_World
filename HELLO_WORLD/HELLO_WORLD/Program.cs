4d02790f71738ae53b9f1d7992b0a0c0706f8260
arne-schlemermeyer <arne.schlemermeyer@student.jade-hs.de>
2021-03-11 10:59:08 +0100

    Update Program.cs

--- HELLO_WORLD/HELLO_WORLD/Program.cs
+++ HELLO_WORLD/HELLO_WORLD/Program.cs
@@ -10,7 +10,19 @@ namespace HELLO_WORLD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // Ausgabe Benutzer 1
+            Console.WriteLine("Hello World von XXX");
+
+            // Ausgabe Benutzer 2
+            Console.WriteLine("Nǐ hǎo, shìjiè");
+
+            // Ausgabe Benutzer 3
+            Console.WriteLine("Hallo Welt von XXX");
+
+            // Ausgabe Benutzer 4
+            Console.WriteLine("marhabaan bialealam");
+
+            // warte bis der User eine Taste drueckt
             Console.ReadKey();
         }
     }
